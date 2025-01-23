689f1d8cd30fa0eb50bc9f7941130e32105968f5
eskilaf <148913314+eskilaf@users.noreply.github.com>
2025-01-23 11:03:22 +0100

    Some small changes

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-	printf("Testing GitHub");
-	printf("Forandring fra Marius sin pc, nytt forsøk");
-	printf("Forandring fra Eskil sin pc");
+	printf("Testing GitHub\n");
+	printf("Forandring fra Marius sin pc, nytt forsøk\n");
+	printf("Forandring fra Eskil sin pc\n");
 	return 0;
 }
